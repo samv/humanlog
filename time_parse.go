2845deeadbd2dcde48c1dc6ad45f407a0795e4bd
Sam Vilain <svilain@tesla.com>
2018-06-22 06:26:41 -0700

    Ingest a 'time.RFC3339Micro' timestamp format
    A project I'm using uses this time format

--- time_parse.go
+++ time_parse.go
@@ -8,6 +8,7 @@ import (
 var formats = []string{
 	"2006-01-02 15:04:05.999999999 -0700 MST",
 	time.RFC3339,
+	"2006-01-02T15:04:05.999999Z07:00",
 	time.RFC3339Nano,
 	time.RFC822,
 	time.RFC822Z,
